b40b2e126fc77b9699077f5d4c89d87474490cc4
aaronzwiebach <aaronz@mit.edu>
2014-01-09 14:32:50 -0500

    Update directoryscript.py

--- directoryscript.py
+++ directoryscript.py
@@ -10,6 +10,8 @@ from os.path import isfile, join
 import csv
 import re
 import itertools
+
+#Import all other scripts.
 from finalgetgeneralinfo import getnewwpmethod, getcharsizeinfo, getlastrevdate, gettimestamp, getimginfo, writelinescript1, writegeneralinfocsv
 from finalgetimginfo import getlivepixinfo, getdeadpixinfo, writelinescript2, writeimginfocsv
 from finalgetxmlinfo import *
@@ -18,9 +20,6 @@ from finalgetxmlinfo import *
 root = "/mnt/nfs6/wikipedia.proj/rawdata/wikidata/"
 
 
-print sys.argv
-print "------------------------------------------------"
-print sys.argv[1]
 playernum = int(sys.argv[1])
 playerlist = open(root + "playerlist.txt","r")
 lines=playerlist.readlines()
@@ -29,6 +28,7 @@ persons = [lines[playernum].strip()]
 
 #For each person in playerlist...
 for person in persons[:1]:
+    
     #create Soup object out of their XML file
     xmlfilepath= root+person+"/"+person+".xml"
     xmlfilehandle= open(xmlfilepath, "r")
@@ -43,11 +43,13 @@ for person in persons[:1]:
     outfile2= open(personalgetimginfo, "w")
     outline2= "revid\timg-name\twidth\tstatus\n"
     outfile2.write(outline2)
+    
     #Create Header line for individuals General Info CSV
     geninfo=root+person+"/csvfiles/"+person+"generalinfo.csv"
     outfile1= open(geninfo, "w")
     outline1= "wpname\tfile-number\tcharsize\timgNum\ttimestamp\tlastrevdate\n"
     outfile1.write(outline1)
+    
     #Create Header line for XML info csv
     getxmlinfo=root+person+"/csvfiles/"+person+"xml.csv"
     outfile3= open(getxmlinfo, "w")
@@ -70,10 +72,8 @@ for person in persons[:1]:
         
 
 
-        #Write t their generalinfo csv and imginfo csv
+        #Write to their generalinfo csv and imginfo csv
         writegeneralinfocsv(person, revisionfile, outfile1, soup)
         writeimginfocsv(person, revisionfile, outfile2, soup)
         
-
-
 
